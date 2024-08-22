072f047a829d2618920e1dc92c3b3d5631b10a37
Jeksos <165167845+Jeksos@users.noreply.github.com>
2024-08-22 16:44:16 +0200

    Create createDB.py

---
+++ createDB.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+connection = sqlite3.connect('schutz.db')
+
+with open('schema SCHUTZ.sql') as f:
+    connection.executescript(f.read())
+
+# cur = connection.cursor()
+# requete = "INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)"
+# cur.execute(requete, ('DUPONT', 'Emilie', '123, Rue des Lilas, 75001 Paris'))
+# cur.execute(requete, ('DUPONT', 'Sonia', '123, Rue des Lilas, 75001 Paris'))
+# cur.execute(requete, ('DUPONT', 'Emilie', '123, Rue des Lilas, 75001 Paris'))
+
+connection.commit()
+connection.close()
