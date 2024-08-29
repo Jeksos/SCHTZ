17e23b109ee02457688a622cb5124a7f538a3cf2
Jeksos <165167845+Jeksos@users.noreply.github.com>
2024-08-29 18:58:20 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -38,7 +38,7 @@ def authentification():
 @app.route('/formulaire_ranger')
 def FormulaireRanger():
     # Afficher la page HTML
-    return render_template('Ranger_un_Composant.html')
+    return render_template('form_ranger.html')
 
 @app.route('/ajouter_composant', methods=['POST'])
 def RangerComposant():
