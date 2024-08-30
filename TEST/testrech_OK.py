86923f32697630ebbca1bb4f287866fc4124db1b
Jeksos <165167845+Jeksos@users.noreply.github.com>
2024-08-30 11:42:12 +0200

    Add files via upload

---
+++ TEST/testrech_OK.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+import time
+
+
+
+# Spécifiez le chemin vers l'exécutable geckodriver
+service = Service(executable_path='/usr/local/bin/geckodriver')
+
+# Initialisez les options Firefox
+options = Options()
+options.add_argument('--headless')  # Pour exécuter Firefox en mode headless
+
+# Initialisez le pilote Firefox avec le service et les options
+driver = webdriver.Firefox(service=service, options=options)
+
+# Ouvrir le site Python
+driver.get("https://jeksos.alwaysdata.net/recherche")
+
+# Afficher le titre de la page
+print(driver.title)
+
+# Trouver le champ reference
+ref = driver.find_element(By.NAME, "reference")
+
+
+# Effacer le contenu s'il y a du texte dedans
+ref.clear()
+
+# Entrer la requête de recherche dans la barre de recherche
+ref.send_keys("3017936")
+
+
+# Trouver l'élément bouton de recherche
+envoie_val = driver.find_element(By.NAME, "recherche")
+
+# Simuler la pression de la touche Entrée
+envoie_val.send_keys(Keys.RETURN)
+
+# attendre le resultat du serveur
+time.sleep(1)
+
+# Trouver le champ resultat ok
+try:
+	resultat = driver.find_element(By.NAME, "resultat_ok")
+	print("Recherche réussie.")
+except:
+    print("Resultat échouée.")
+
+# Trouver le champ resultat nok
+# try:
+# 	resultat = driver.find_element(By.NAME, "resultat_nok")
+# 	print("Pas de resultat.")
+# except e:
+#     print("Erreur de recherche.")
+
+
+# Fermer le navigateur
+driver.close()
