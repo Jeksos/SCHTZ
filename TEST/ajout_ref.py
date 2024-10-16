ea5c766cc512c7970e6c75816b5c2561501aebd3
Jeksos <165167845+Jeksos@users.noreply.github.com>
2024-10-16 15:15:40 +0200

    Update ajout_ref.py

--- TEST/ajout_ref.py
+++ TEST/ajout_ref.py
@@ -24,31 +24,33 @@ time.sleep(1)
 # Trouver les champs du formulaire et entrer les valeurs
 
 # Champ Allée
-allee = driver.find_element(By.NAME, "Allée")
-allee.select_by_value("A")   # Sélectionner l'allée A
+allee = driver.find_element(By.ID, "allee")
+allee.send_keys('I')   # Sélectionner l'allée A
 
 # Champ Emplacement
-emplacement = driver.find_element(By.NAME, "emplacement")
-emplacement.select_by_value("100")  # Sélectionner l'emplacement 100
+emplacement = driver.find_element(By.ID, "emplacement")
+emplacement.send_keys('1')  # Sélectionner l'emplacement 100
 
 # Champ Référence
-reference = driver.find_element(By.NAME, "reference")
+reference = driver.find_element(By.ID, "reference")
 reference.clear()
 reference.send_keys("3019746")  # Entrer la référence à ajouter
 
 # Champ Date
-date = driver.find_element(By.NAME, "date")
-date.send_keys("2024-09-05")  # Entrer une date valide
+date = driver.find_element(By.ID, "date")
+date.send_keys("05/09/2024")  # Entrer une date valide
 
 # Soumettre le formulaire
 submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
 submit_button.click()
 
 # Attendre la réponse du serveur après soumission
-time.sleep(2)
+time.sleep(5)
 
 # Vérifier si l'ajout a réussi (par exemple, si un message de succès est affiché)
 try:
+    success_message = driver.find_element(By.XPATH, "//div[@id='message']")
+    print(success_message.text)
     success_message = driver.find_element(By.XPATH, "//div[@id='message' and contains(text(), 'succès')]")
     print("Ajout de la référence réussi.")
 except:
